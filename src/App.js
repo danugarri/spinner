3ae9a1c11cdcd995ce4d722c4188cff94d923a97
daniel <danugarri@gmail.com>
2021-12-08 08:56:21 +0100

    spinner workin with fake data emulating asynchronus function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Spinner } from "./components/Spinner/Spinner";
 import './App.css';
 
 function App() {
+
+  const [spinner, setSpinner] = useState(true);
+  const [dataTest, setDataTest ] = useState(<></>);
+    const data = (<table>
+      <tr>
+      <th> Nombre </th>
+      <th> Apellido </ th>
+      <th> Nota </ th>
+      <th> Convocatoria </ th>
+      </ tr>
+      <tr>
+      <td> Maria </ td>
+      <td> Campos </ td>
+      <td> 8.3 </ td>
+      <td> EVA1 </ td>
+      </ tr>
+      <tr>
+      <td> Jose </ td>
+      <td> Garcia </ td>
+      <td> 6.7 </ td>
+      <td> EVA1 </ td>
+      </ tr>
+      <tr>
+      <td> Marcos </ td>
+      <td> Galvez </ td>
+      <td> 3.5 </ td>
+      <td> EVA1 </ td>
+      </ tr>
+      </table>)
+
+  const funcionAsincrona = () => {
+    setSpinner(false);
+  }
+   const showData = () => {
+     setDataTest(data);
+    }
+  useEffect(() => {
+   
+    setTimeout(showData,4000);
+    setTimeout(funcionAsincrona,4000)
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     {
+     spinner && <Spinner />
+    }
+    {
+    !spinner && dataTest}
     </div>
   );
 }
---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Spinner.css';
+
+
+export const Spinner = () => {
+
+
+    return(
+        <section id="container">
+        <div className="loader1"></div>
+        <div className="loader2"></div>
+        </section>
+
+    )
+
+}
